bca28d4a9244564dd6918eff48b55838eb42eeb4
MatteoMicozzi <micozzimatteo@yahoo.it>
2022-03-11 13:29:11 +0000

    Added test for Club #list_players_from(nationality).

--- spec/club_spec.rb
+++ spec/club_spec.rb
@@ -30,4 +30,12 @@ describe Club do
       expect(subject.list_players_ranked('Unranked')).to eq("Rank Position | Name Surname | Age | Nationality | Ranking | Score\n1 | Chill Nill | 31 | Mexican | Unranked | 1200\n2 | Tom Huge | 38 | British | Unranked | 1200\n3 | Bill Ford | 38 | American | Unranked | 1200")
     end
   end
+  describe '#list_players_from' do
+    it "will return a list of all players from Mexican nationality" do
+      subject.new_player('Tom', 'Huge', 'British', '10-07-1983')
+      subject.new_player('Bill', 'Ford', "American", '11-09-1983')
+      subject.new_player('Chill', 'Nill', "Mexican", '28-03-1990')
+      expect(subject.list_players_from('Mexican')).to eq("Rank Position | Name Surname | Age | Nationality | Ranking | Score\n1 | Chill Nill | 31 | Mexican | Unranked | 1200")
+    end
+  end
 end
